12ddb881bfb6e800145f2668bebbc1455dfd2316
Jacob Prudent <jake.prudent@gmail.com>
2020-02-03 21:14:50 -0800

    nothing major

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,15 +1,14 @@
 import React from 'react';
 // import Link from "@material-ui/core/Link";
-import { Link } from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 function Navbar() {
-    return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/sign-in">Sign-In</Link>
-            
-        </nav>
-    )
+	return (
+		<nav>
+			<Link to="/">Home</Link>
+			<Link to="/sign-in">Sign-In</Link>
+		</nav>
+	);
 }
 
 export default Navbar;
