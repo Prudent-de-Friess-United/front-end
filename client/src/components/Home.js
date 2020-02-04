c328298507f8f1568f26cee3c1a0d9c40aad8c2e
Tyler de Friess <tyler.defriess@gmail.com>
2020-02-04 17:34:50 -0500

    Added axios request for user`s items

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,24 +1,33 @@
-import React, {useReducer, useState, useContext} from 'react';
+import React, {useReducer, useState, useEffect, useContext} from 'react';
 import AppContext from '../contexts/AppContext';
 import ItemCard from './ItemCard';
 import Axios from 'axios';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 function Home() {
-	const {appState, dispatch} = useContext(AppContext);
-	let items = {};
-	// Axios
-	//     .get('https://african-market-lambda.herokuapp.com/items/')
-	//     .then(res => {
-	//         console.log(res);
-	//     })
-	//     .catch(err => {
-	//         console.log('The data was not returned', err);
-	//     });
+    const {appState, dispatch} = useContext(AppContext);
+    const {items, setItems} = useState([]);
+    
+    
+    useEffect(() => {
+        axiosWithAuth()
+	    .get('https://african-market-lambda.herokuapp.com/items/')
+	    .then(res => {
+            console.log('Item List:', res.data);
+            setItems(res.data);
+	    })
+	    .catch(err => {
+	        console.log('The data was not returned', err);
+        })
+        .finally(console.log("Finally:", items));
+    }, [])	
 
 	return (
 		<div>
 			<h1>Welcome to your Dashboard.</h1>
-			<ItemCard />
+                {[].map(item => {
+                    return <ItemCard key={item.id} item={item}/>
+                })}
 		</div>
 	);
 }
