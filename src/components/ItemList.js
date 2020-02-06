53147ad47d671d7600398ea322315bfab2ae7858
Tyler de Friess <tyler.defriess@gmail.com>
2020-02-06 13:20:51 -0500

    Added search functionality to item list--Tyler de Friess

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,10 +4,28 @@ import AppContext from '../contexts/AppContext';
 import ItemCard from './ItemCard';
 import NewItem from './NewItem';
 import {axiosWithAuth} from '../utils/axiosWithAuth';
+import TextField from '@material-ui/core/TextField';
 
 function ItemList() {
 	const {appState, dispatch} = useContext(AppContext);
 	const [items, setItems] = useState([]);
+	const [search, setSearch] = useState('');
+	const [searched, setSearched] = useState(false);
+	const [results, setResults] = useState([]);
+
+	useEffect(() => {
+		const searchResults = items.filter(item => item.name.toLowerCase().includes(search.toLowerCase()))
+		setResults(searchResults);
+	}, [search])
+
+	const handleSearchChanges = event => {
+		setSearch(event.target.value);
+		if (event.target.value === ''){
+			setSearched(false);
+		} else {
+			setSearched(true);
+		}
+	}
 
 	useEffect(() => {
 		axiosWithAuth()
@@ -25,8 +43,9 @@ function ItemList() {
 	return (
 		<div>
 			<h1>Welcome to your Dashboard.</h1>
-			{items.map(item => {
-				console.log(item);
+			<TextField id="search" label="Search" value={search} onChange={handleSearchChanges}></TextField>
+			{(!searched ? items : results).map(item => {
+				// console.log(item);
 				return <ItemCard key={item.id} item={item} />;
 			})}
 		</div>
