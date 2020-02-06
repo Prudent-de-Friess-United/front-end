04058c4619afa9bedb0733ab8d8a9e17293c5a18
Jacob Prudent <jake.prudent@gmail.com>
2020-02-06 12:41:52 -0800

    Home photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,8 @@
 import React, {useReducer, useContext} from 'react';
-
+import Image from '../images/Home.jpg';
 function Home() {
-	
+	const styles = {};
 
-	return (
-		<div>
-			
-		</div>
-	);
+	return <div></div>;
 }
 export default Home;
